60e021e9063cfa2dd45f821cd545392e69a3bd72
JordanAbbott169024 <44343346+JordanAbbott169024@users.noreply.github.com>
2020-10-01 16:51:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require("express"); // Express web server framework
-var request = require("request"); // "Request" library
+var express = require("express");
+var request = require("request");
 var cors = require("cors");
 var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
 
-var client_id = process.env.CLIENT_ID; // Your client id
-var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = 'https://blooming-savannah-31882.herokuapp.com/callback'; // Your redirect uri
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
+var redirect_uri = 'https://blooming-savannah-31882.herokuapp.com/callback';
 
 var generateRandomString = function(length) {
   var text = '';
