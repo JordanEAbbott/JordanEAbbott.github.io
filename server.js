fa3ea4de3956ba3e18f559fbe122320b5607e815
JordanAbbott169024 <44343346+JordanAbbott169024@users.noreply.github.com>
2020-10-02 14:47:18 +0100

    Added log_details button

--- server.js
+++ server.js
@@ -86,30 +86,17 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
-        var user_id;
         var options = {
           url: 'https://api.spotify.com/v1/me',
           headers: { 'Authorization': 'Bearer ' + access_token },
           json: true
         };
 
+        var user_id;
         request.get(options, function(error, response, body) {
           console.log(body);
-          var user_id = body.id;
-        });
-
-        var last_liked = {
-          url: 'https://api.spotify.com/v1/me/tracks?offset=0&limit=50',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json:true
-        };
-
-        request.get(last_liked, function(error, response, body) {
-          client.query(`INSERT INTO users VALUES (${user_id}, \'example_friends\', \'example_songs\');`, (err, res) => {
-            if (err) throw err;
-            client.end();
-          });
-          console.log(body);
+          user_id = body.id;
+          consloe.log(user_id)
         });
 
         res.redirect('/#' +
@@ -148,6 +135,56 @@ app.get('/refresh_token', function(req, res) {
       });
     }
   });
+});
+
+app.get('/log_details', function(req, res) {
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var saved_tracks = {
+          url: 'https://api.spotify.com/v1/me/tracks?offset=0&limit=50',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        request.get(saved_tracks, function (error, response, body) {
+          client.query(`INSERT INTO users VALUES (${user_id}, \'example_friends\', \'example_songs\');`, (err, res) => {
+            if (err) throw err;
+            client.end();
+          });
+          console.log(body);
+        });
+      }
+    });
+  };
 });
 
 console.log('Listening on 8888');
