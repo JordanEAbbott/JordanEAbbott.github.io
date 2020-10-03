517a06584c8bf0ca07aebe7b8a3a85313c5c0a44
JordanAbbott169024 <44343346+JordanAbbott169024@users.noreply.github.com>
2020-10-03 16:06:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ client.connect();
 var client_id = process.env.CLIENT_ID;
 var client_secret = process.env.CLIENT_SECRET;
 var redirect_uri = 'https://spotifind.co.uk/callback/';
+var code;
 
 var generateRandomString = function(length) {
 
@@ -139,68 +140,57 @@ app.get('/refresh_token', function(req, res) {
 
 app.get('/log_details', function(req, res) {
 
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
+console.log('Authorization code:' + code);
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    json: true
+  };
 
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-    console.log(state);
-    console.log(storedState);
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-    request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function(error, response, body) {
 
-      if (!error && response.statusCode === 200) {
-        var access_token = body.access_token;
-        console.log('request made')
-        var user_data = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-        var user_id = [];
-        request.get(user_data, function(error, response, body) {
-          console.log(body);
-          user_id.push(body.id)
-          console.log(user_id)
-        });
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
 
-        console.log(user_id[0]);
-        var saved_tracks = {
-          url: 'https://api.spotify.com/v1/me/tracks?offset=0&limit=50',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        request.get(saved_tracks, function (error, response, body) {
-          client.query(`INSERT INTO users VALUES (${user_id[0]}, \'example_friends\', \'example_songs\');`, (err, res) => {
-            if (err) throw err;
-            client.end();
-          });
-          console.log(body);
-        });
-      } else {
-        console.log(error);
-        console.log(response.statusCode);
+      var user_data = {
+        url: 'https://api.spotify.com/v1/me',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        json: true
       };
-    });
-  }
+      var user_id = [];
+      request.get(user_data, function(error, response, body) {
+        console.log(body);
+        user_id.push(body.id)
+        console.log(user_id)
+      });
+
+      console.log(user_id[0]);
+      var saved_tracks = {
+        url: 'https://api.spotify.com/v1/me/tracks?offset=0&limit=50',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        json: true
+      };
+
+      request.get(saved_tracks, function (error, response, body) {
+        client.query(`INSERT INTO users VALUES (${user_id[0]}, \'example_friends\', \'example_songs\');`, (err, res) => {
+          if (err) throw err;
+          client.end();
+        });
+        console.log(body);
+      });
+    } else {
+      console.log(error);
+      console.log(response.statusCode);
+    };
+
+  });
 });
 
 console.log('Listening on 8888');
